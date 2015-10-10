6a9e4f7930d1acc23a7723b13fdcb95fc82e84c5
xiaodao <lychees67@gmail.com>
2015-10-10 21:16:13 +0800

    123

--- Archive/Regional/readme.md
+++ Archive/Regional/readme.md
@@ -1,3 +1,143 @@
+# 2014 ACM/ICPC 中国大陆赛区
+
+## 牡丹江
+
+A.
+签到
+B.
+树的直径，二分答案
+C.
+恶模拟
+D.
+DP、期望
+E. Excavator Contest
+构造
+F. Fiber-optic Network
+树 DP、容斥原理
+G. Garden and Sprinklers
+计算几何
+H. Hierarchical Notation
+模拟
+J. Jacobi Pattern
+？？字符串？？
+K.
+贪心
+
+
+## 鞍山
+
+A.
+???
+B. 
+大模拟
+C. 
+补集计数、容斥原理
+D. 
+签到
+E. 
+简单 DP
+F. 
+???
+G. 
+最小割
+H. 
+搜索？
+I. 
+签到
+J. 
+SCDP？
+K. Colorful Toy
+计算几何、polya 计数法
+L. Trie
+AC 自动机，fail 树，dfs 序列，链并，树状数组、树链剖分
+
+## 西安
+？？？
+
+A.
+签到
+B.
+？？？
+C.
+最大密度子图
+D.
+？？？
+E.
+计算几何
+F. Color
+容斥原理
+G. The Problem to Slow Down You 
+字符串、回文树、Manacher、Hash
+H. The Problem to Make You Happy
+博弈 DP
+I. International Collegiate Routing Contest
+Trie 树、bitset、dfs
+J. Unlimited Battery Works 
+？？？
+K. Last Defence
+gcd
+
+
+## 北京
+
+A.
+签到
+B.
+构造
+C.
+数论
+D.
+DP、正难则反
+E.
+扫描线
+F.
+期望
+G.
+可持久化 Treap
+H.
+背包、xor
+I.
+计算几何
+J.
+？？？
+K. Bro Sorting
+树状数组
+
+## 上海赛区
+http://acmicpc.info/archives/1881
+
+A. The Matrix Revolutions
+http://www.cnblogs.com/oyking/p/4598601.html
+？？？矩阵树定理
+
+B. Rotation
+模拟。
+
+C. Seat Arrangement
+？？？
+D. BombX
+离散化、线段树
+E. Block the Roads
+？？
+F. Room Assignment
+组合 DP
+G. Game
+？？？
+H. Lawn Mower
+？？？
+I. Defeat the Enemy
+贪心、排序
+J. World Cup
+贪心
+K. LRIP
+树分治
+L. Top ACMers
+？
+
+
+
+
+To do list:
 Beijing
 04：
 FWT
--- Workspace/B.cpp
+++ Workspace/B.cpp
@@ -1,134 +1,113 @@
-struct SAM
-{
-    SAM *son[4],*pre;
-    int len;
-    int ok;
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <unordered_map>
 
-    void init()
-    {
-        clr(son,0);
-        pre=0;
-        ok=0;
+#define REP(i, n) for (int i=0;i<n;++i)
+#define ECH(it, A) for (__typeof(A.begin()) it=A.begin();it!=A.end();++it)
+#define fi first
+#define se second
+using namespace std;
+
+const int N = int(2e5) + 9, Z = 26;
+
+namespace SAM{
+
+    int trans[N][Z], len[N], fail[N], cnt[N], tail, tot;
+    char str[N]; int st[N];
+
+    inline int new_node(){
+        memset(trans[tot], 0, sizeof(*trans));
+        fail[tot] = 0;
+        cnt[tot] = 0;
+        tail = tot;
+        return tot++;
     }
-};
 
-
-SAM sam[N],*head,*last;
-int cnt;
-
-
-void initSam()
-{
-    head=last=&sam[0];
-    head->init();
-    cnt=1;
-}
-
-
-int get(char x)
-{
-    if(x=='A') return 0;
-    if(x=='T') return 1;
-    if(x=='G') return 2;
-    return 3;
-}
-
-void insert(int x)
-{
-    SAM *p=&sam[cnt++],*u=last;
-
-    p->init();
-
-    p->len=last->len+1;
-    last=p;
-    for(;u&&!u->son[x];u=u->pre) u->son[x]=p;
-    if(!u) p->pre=head;
-    else if(u->son[x]->len==u->len+1) p->pre=u->son[x];
-    else
-    {
-        SAM *r=&sam[cnt++],*q=u->son[x];
-        *r=*q; r->len=u->len+1;
-        p->pre=q->pre=r;
-        for(;u&&u->son[x]==q;u=u->pre) u->son[x]=r;
+    inline int new_node(int u){
+        memcpy(trans[tot], trans[u], sizeof(*trans));
+        fail[tot] = fail[u];
+        cnt[tot] = cnt[u];
+        return tot++;
     }
-}
 
+    #define v trans[u][c]
+    #define p fail[u]
+    #define pp fail[uu]
 
-char s[N*2];
-int cur;
-
-int start[N],len[N];
-
-vector<pair<int,int> > V[N];
-
-int ans[N];
-
-
-map<int,int> mp;
-
-
-int n,m;
-
-int main()
-{
-//  FFF;
-
-    while(scanf("%d%d",&n,&m)!=-1)
-    {
-        cur=0;
-        int i;
-        for(i=1;i<=n;i++)
-        {
-            scanf("%s",s+cur);
-            start[i]=cur;
-            len[i]=strlen(s+cur);
-            cur+=len[i]+3;
-        }
-        for(i=1;i<=m;i++)
-        {
-            int u,v;
-            scanf("%d%d",&u,&v);
-            V[u].pb(MP(v,i));
-        }
-
-
-        for(i=1;i<=n;i++) if(SZ(V[i])>0)
-        {
-            int j;
-            initSam();
-            for(j=start[i];s[j];j++)  insert(get(s[j]));
-            SAM *p=last;
-            while(p!=NULL) p->ok=1,p=p->pre;
-
-
-            mp.clear();
-            for(j=0;j<SZ(V[i]);j++)
-            {
-                int k=V[i][j].first;
-                int id=V[i][j].second;
-                if(mp.count(k))
-                {
-                    ans[id]=mp[k];
-                    continue;
-                }
-                SAM *p=head;
-                int t,tmp=0,cur=0;
-                for(t=start[k];s[t];t++)
-                {
-                    int x=get(s[t]);
-                    if(p->son[x])
-                    {
-                        cur++;
-                        p=p->son[x];
-                        if(p->ok) tmp=max(tmp,cur);
-                    }
-                    else break;
-                }
-                mp[k]=tmp;
-                ans[id]=tmp;
+    int Ext(int c){
+        int u = tail, uu = new_node(); len[uu] = len[u] + 1;
+        while (u && !v) v = uu, u = p; // 向上遍历没有 c-转移 的祖先 ..
+        if (!u && !v) v = uu, pp = 0;
+        else{
+            if (len[v] == len[u] + 1) pp = v;
+            else{
+                int _v = v, vv = new_node(_v); len[vv] = len[u] + 1;
+                fail[_v] = pp = vv;
+                while (v == _v) v = vv, u = p;
+                if (!u && v == _v) v = vv;
             }
-            V[i].clear();
         }
-        for(i=1;i<=m;i++) printf("%d\n",ans[i]);
+        return uu;
+    }
+
+#define c (*cur - 'A')
+
+    void Init(char *cur){
+        tot = 0; new_node(); for (;*cur;++cur) Ext(c);
+        for (int u = tail; u; u = p) cnt[u] = 1;
+    }
+
+    int Query(char *cur){
+        int u = 0, i = 0, z = 0; for (;*cur;++cur){
+            if (!v) break;
+            ++i; u = v;
+            if (cnt[u]) z = i;
+        }
+        return z;
+    }
+
+} using namespace SAM;
+
+vector<pair<int, int> > qry[N];
+int ans[N]; int n, m;
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+#endif
+
+    while (~scanf("%d %d", &n, &m)){
+
+        REP(i, n){
+            scanf("%s", str + st[i]);
+            st[i+1] = st[i] + strlen(str + st[i]) + 1;
+            qry[i].clear();
+        }
+
+        REP(i, m){
+            int l, r; scanf("%d %d", &l, &r); --l, --r;
+            qry[l].push_back(make_pair(r, i));
+        }
+
+        REP(l, n) if (qry[l].size()){
+            unordered_map<int, int> cache;
+            Init(str + st[l]);
+            ECH(it, qry[l]){
+                int r = it->fi, id = it->se;
+                if (cache.find(r) != cache.end()) ans[id] = cache[r];
+                else{
+                    ans[id] = Query(str + st[r]);
+                    cache[r] = ans[id];
+                }
+            }
+        }
+
+        REP(i, m) printf("%d\n", ans[i]);
     }
 }
