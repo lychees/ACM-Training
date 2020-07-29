c91aa6cdd47f36b53e2a1dee8cd33f68c661ec7c
xiaodao <lychees67@gmail.com>
2020-07-29 12:39:29 -0700

    go

--- Workspace/AA.cpp
+++ Workspace/AA.cpp
@@ -1,112 +1,148 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<algorithm>
+#include <cstdio>
+#include <cctype>
+#include <algorithm>
+#include <iostream>
+
 using namespace std;
-#define MAX 1010000
-inline int read()
+namespace Zeonfai
 {
-    int x=0;bool t=false;char ch=getchar();
-    while((ch<'0'||ch>'9')&&ch!='-')ch=getchar();
-    if(ch=='-')t=true,ch=getchar();
-    while(ch<='9'&&ch>='0')x=x*10+ch-48,ch=getchar();
-    return t?-x:x;
-}
-int n,m;char ch[MAX];
-long long ans;
-struct Node
-{
-    int son[26];
-    int len,ff;
-}t[MAX];
-int last=1,tot=1,lt[MAX];
-void extend(int c)
-{
-    int p=last,np=++tot;last=tot;
-    t[np].len=t[p].len+1;
-    while(p&&!t[p].son[c])t[p].son[c]=np,p=t[p].ff;
-    if(!p)t[np].ff=1;
-    else
+    inline int getInt()
     {
-        int q=t[p].son[c];
-        if(t[q].len==t[p].len+1)t[np].ff=q;
-        else
+        int a = 0, sgn = 1; char c;
+        while(! isdigit(c = getchar())) if(c == '-') sgn *= -1;
+        while(isdigit(c)) a = a * 10 + c - '0', c = getchar();
+        return a * sgn;
+    }
+}
+const int N = (int)1e5, M = (int)2e5, INF = (int)2e9, T = (int)1e5;
+int n, m;
+int ans[T + 1];
+struct edge
+{
+    int u, v, L, R;
+    inline int operator <(const edge &a) const {return L == a.L ? R < a.R : L < a.L;}
+}edg[M];
+struct linkCutTree
+{
+    int tp;
+    struct node
+    {
+        int pre, suc[2], isRoot, rev;
+        int w, mn, sz;
+        inline node() {pre = -1; for(int i = 0; i < 2; ++ i) suc[i] = -1; isRoot = 1; rev = 0; sz = 1;}
+    }nd[N + 1 + M];
+    inline void initialize()
+    {
+        for(int i = 1; i <= n; ++ i) nd[i].w = INF, nd[i].mn = i;
+        tp = n + 1;
+    }
+    inline void pushDown(int u)
+    {
+        if(! nd[u].isRoot) pushDown(nd[u].pre);
+        if(nd[u].rev)
         {
-            int nq=++tot;
-            t[nq]=t[q];t[nq].len=t[p].len+1;
-            t[q].ff=t[np].ff=nq;
-            while(p&&t[p].son[c]==q)t[p].son[c]=nq,p=t[p].ff;
+            for(int i = 0; i < 2; ++ i) if(~ nd[u].suc[i])
+                swap(nd[nd[u].suc[i]].suc[0], nd[nd[u].suc[i]].suc[1]), nd[nd[u].suc[i]].rev ^= 1;
+            nd[u].rev = 0;
         }
     }
-}
-struct Line{int v,next;}e[MAX];
-int h[MAX],cnt=1;
-inline void Add(int u,int v){e[cnt]=(Line){v,h[u]};h[u]=cnt++;}
-int dfn[MAX],low[MAX],dep[MAX],fa[MAX],tim,size[MAX],hson[MAX],top[MAX];
-void dfs1(int u,int ff)
-{
-    size[u]=1;dep[u]=dep[ff]+1;fa[u]=ff;
-    for(int i=h[u];i;i=e[i].next)
+    inline int getRelation(int u) {return u == nd[nd[u].pre].suc[1];}
+    inline void update(int u)
     {
-        int v=e[i].v;
-        dfs1(v,u);size[u]+=size[v];
-        if(size[v]>size[hson[u]])hson[u]=v;
+        nd[u].mn = u; nd[u].sz = 1;
+        for(int i = 0; i < 2; ++ i) if(~ nd[u].suc[i])
+        {
+            if(nd[nd[nd[u].suc[i]].mn].w < nd[nd[u].mn].w) nd[u].mn = nd[nd[u].suc[i]].mn;
+            nd[u].sz += nd[nd[u].suc[i]].sz;
+        }
     }
-}
-void dfs2(int u,int tp)
-{
-    dfn[u]=++tim;top[u]=tp;
-    if(hson[u])dfs2(hson[u],tp);
-    for(int i=h[u];i;i=e[i].next)
-        if(e[i].v!=hson[u])dfs2(e[i].v,e[i].v);
-    low[u]=tim;
-}
-int LCA(int u,int v)
-{
-    while(top[u]^top[v])(dep[top[u]]<dep[top[v]])?v=fa[top[v]]:u=fa[top[u]];
-    return dep[u]<dep[v]?u:v;
-}
-int p[MAX],S[MAX];
-bool vis[MAX];
-bool cmp(int a,int b){return dfn[a]<dfn[b];}
-int dp(int u)
-{
-    int ret=0;if(vis[u])ret=1;
-    for(int i=h[u];i;i=e[i].next)
+    inline void rotate(int u)
     {
-        int d=dp(e[i].v);
-        ans+=1ll*t[u].len*d*ret;
-        ret+=d;
+        int pre = nd[u].pre, prepre = nd[pre].pre, k = getRelation(u);
+        nd[pre].suc[k] = nd[u].suc[k ^ 1]; if(~ nd[u].suc[k ^ 1]) nd[nd[u].suc[k ^ 1]].pre = pre;
+        nd[u].pre = prepre; if(! nd[pre].isRoot) nd[prepre].suc[getRelation(pre)] = u;
+        nd[pre].pre = u; nd[u].suc[k ^ 1] = pre;
+        if(nd[pre].isRoot) nd[pre].isRoot = 0, nd[u].isRoot = 1;
+        update(pre); update(u);
     }
-    return ret;
-}
+    inline void splay(int u)
+    {
+        pushDown(u);
+        while(! nd[u].isRoot)
+        {
+            if(! nd[nd[u].pre].isRoot) rotate(getRelation(u) == getRelation(nd[u].pre) ? nd[u].pre : u);
+            rotate(u);
+        }
+    }
+    inline void access(int u)
+    {
+        splay(u);
+        if(~ nd[u].suc[1])
+        {
+            nd[nd[u].suc[1]].isRoot = 1;
+            nd[u].suc[1] = -1; update(u);
+        }
+        while(~ nd[u].pre)
+        {
+            int pre = nd[u].pre;
+            splay(pre);
+            if(~ nd[pre].suc[1])
+            {
+                nd[nd[pre].suc[1]].isRoot = 1;
+                nd[pre].suc[1] = -1; update(pre);
+            }
+            nd[pre].suc[1] = u; nd[u].isRoot = 0; update(pre);
+            splay(u);
+        }
+    }
+    inline void makeRoot(int u)
+    {
+        access(u); swap(nd[u].suc[0], nd[u].suc[1]); nd[u].rev ^= 1;
+    }
+    inline int get(int u)
+    {
+        access(u);
+        while(~ nd[u].suc[0]) u = nd[u].suc[0];
+        return u;
+    }
+    inline int newNode(int w) {nd[tp].w = w; nd[tp].mn = tp; return tp ++;}
+    inline void link(int id)
+    {
+        int u = edg[id].u, v = edg[id].v;
+        if(get(u) == get(v))
+        {
+            makeRoot(u); access(v);
+            int x = nd[v].mn;
+
+            cout << nd[x].w << endl;
+
+            if(nd[x].w > edg[id].L && nd[v].sz + 1 >> 1 & 1) for(int i = edg[id].L + 1; i <= min(nd[x].w, edg[id].R); ++ i) ans[i] = 1;
+
+
+
+            if(nd[x].w > edg[id].R) return;
+            access(x); nd[nd[x].suc[0]].pre = -1; nd[nd[x].suc[0]].isRoot = 1; nd[x].suc[0] = -1;
+            makeRoot(v); access(x); nd[nd[x].suc[0]].pre = -1; nd[nd[x].suc[0]].isRoot = 1; nd[x].suc[0] = -1;
+        }
+        makeRoot(u); makeRoot(v);
+        int x = newNode(edg[id].R); nd[x].pre = u; nd[v].pre = x;
+        return;
+    }
+}LCT;
 int main()
 {
 
 #ifndef ONLINE_JUDGE
     freopen("in.txt", "r", stdin);
-    // freopen("out.txt", "w", stdout);
+    //freopen("out.txt", "w", stdout);
 #endif
 
-    n=read();m=read();
-    scanf("%s",ch+1);reverse(&ch[1],&ch[n+1]);
-    for(int i=1;i<=n;++i)extend(ch[i]-97),lt[i]=last;
-    for(int i=1;i<=tot;++i)if(t[i].ff)Add(t[i].ff,i);
-    dfs1(1,0);dfs2(1,1);memset(h,0,sizeof(h));cnt=1;
-    while(m--)
-    {
-        int t=read(),top=0;cnt=0;
-        for(int i=1;i<=t;++i)vis[p[i]=lt[n-read()+1]]=true;
-        sort(&p[1],&p[t+1],cmp);
-        for(int i=t;i>1;--i)p[++t]=LCA(p[i],p[i-1]);
-        sort(&p[1],&p[t+1],cmp);t=unique(&p[1],&p[t+1])-p-1;
-        for(int i=1;i<=t;++i)
-        {
-            while(top&&low[S[top]]<dfn[p[i]])--top;
-            Add(S[top],p[i]);S[++top]=p[i];
-        }
-        ans=0;dp(p[1]);printf("%lld\n",ans);
-        for(int i=1;i<=t;++i)h[p[i]]=0,vis[p[i]]=false;
-    }
-    return 0;
+
+    using namespace Zeonfai;
+    n = getInt(), m = getInt(); int T = getInt();
+    LCT.initialize();
+    for(int i = 0; i < m; ++ i) edg[i].u = getInt(), edg[i].v = getInt(), edg[i].L = getInt(), edg[i].R = getInt();
+    sort(edg, edg + m);
+    for(int i = 0; i < m; ++ i) LCT.link(i);
+    for(int i = 1; i <= T; ++ i) puts(! ans[i] ? "Yes" : "No");
 }
