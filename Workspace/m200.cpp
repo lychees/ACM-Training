986902764156713a0ee90a1309ccd5d3b39008ab
xiaodao <lychees67@gmail.com>
2021-09-17 19:16:56 +0800

    go

--- Workspace/m200.cpp
+++ Workspace/m200.cpp
@@ -23,19 +23,20 @@
 
 using namespace std;
 
-double m, n;
-double ans;
+const long long MAX = 1e7;
+long long n, m, c, v;
+long long dp[MAX];
 
 int main(){
 
     #ifndef ONLINE_JUDGE
-    freopen("/Users/minakokojima/Documents/GitHub/ACM-Training/Workspace/in.txt", "r", stdin);
+    freopen("in.txt", "r", stdin);
     //freopen("/Users/minakokojima/Documents/GitHub/ACM-Training/Workspace/out.txt", "w", stdout);
 #endif
-
-        cin >> m >> n;
-        for(int i = 0; i <= m; i++) ans += ((i-1)*2+1)*i;
-        for(int i = 1; i < n; i++) m *= m;
-        ans *= 1/m;
-        cout << ans << endl;
+    cin >> n >> m;
+    while(m--){
+        cin >> c >> v;
+        for(long long i = c; i <= n; i++) dp[i] = max(dp[i], dp[i-c] + v);
+    }
+    cout << dp[n];
 }
