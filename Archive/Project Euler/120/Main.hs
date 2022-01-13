44d9a1f435a8080046c3f1fe03b546dae2fd6922
xiaodao <lychees67@gmail.com>
2022-01-13 18:39:50 +0800

    Update Main.hs

--- Archive/Project Euler/120/Main.hs
+++ Archive/Project Euler/120/Main.hs
@@ -1,2 +1,19 @@
+import Data.List
+
 p120 = sum [(a-1)`div`2*2*a | a <- [3..1000]]
-main = print $ p120
+
+s1 n m = ff n m 
+    where
+        ff n m | m == 0 = 1
+               | m > n = 0
+               | otherwise = maybe undefined id $ lookup (n, m) h
+        h                  = map g [(x, y) | x <- [0..n], y <- [0..m]]
+        g (n, m)           = ((n, m), gg n m)
+        gg n m             = n * (ff (n-1) (m-1)) + (ff (n-1) (m)) 
+ 
+p121 n = div total win 
+    where 
+        total = product [1..n+1]
+        win   = sum $ map (s1 n) [x | x <- [0..(div n 2)]]
+
+main = print $ p121 15
