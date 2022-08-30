3deade124e22797280fefae139810d0ff3c2cd51
xiaodao <lychees67@gmail.com>
2022-08-30 17:34:21 +0800

    go

--- Workspace/Cpp/B.cpp
+++ Workspace/Cpp/B.cpp
@@ -1,56 +1,481 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long double db;
-const int N=45;
-const db eps=1e-9;
-int sgn(db x){return (-eps<=x&&x<=eps)?0:(x>0?1:-1);}
-int n,m,t,id[N];db res[N];
-db a[N][N];
-int rd(){int x;cin>>x;return x;}
-void pivot(int x,int y){
-	swap(id[n+x],id[y]);
-	db d=-a[x][y];
-	for(int i=0;i<=n;i++)a[x][i]/=d;
-	a[x][y]=-1./d;
-	for(int i=0;i<=m;i++)if(i!=x&&sgn(a[i][y])){
-		d=a[i][y],a[i][y]=0;
-		for(int j=0;j<=n;j++)a[i][j]+=a[x][j]*d;
-	}
-}
-void simplex(){
-	iota(id+1,id+1+n+m,1);
-	while(1){
-		int u=1,v=0;
-		for(int i=1;i<=m;i++)if(a[i][0]<a[u][0])u=i;
-		if(sgn(a[u][0])>=0)break;
-		for(int j=1;j<=n;j++)if(sgn(a[u][j])>0&&(!v||(id[j]>id[v])))v=j;
-		if(!v)puts("Infeasible"),exit(0);
-		pivot(u,v);
-	}
-	while(1){
-		int u=0,v=1;db mn=1e9;
-		for(int j=1;j<=n;j++)if(a[0][j]>a[0][v])v=j;
-		if(sgn(a[0][v])<=0)break;
-		for(int i=1;i<=m;i++)if(sgn(a[i][v])<0){
-			db t=-a[i][0]/a[i][v];
-			if(sgn(t-mn)<0||(sgn(t-mn)==0&&(!u||id[i]>id[u])))mn=t,u=i;
-		}
-		if(!u)puts("Unbounded"),exit(0);
-		pivot(u,v);
-	}
-}
-signed main(){
-	cin>>n>>m>>t;
-	for(int i=1;i<=n;i++)a[0][i]=rd();
-	for(int i=1;i<=m;i++){
-		for(int j=1;j<=n;j++)a[i][j]=-rd();
-		a[i][0]=rd();
-	}
-	simplex();
-	printf("%.10Lf\n",a[0][0]);
-	if(t){
-		for(int i=1;i<=m;i++)res[id[i+n]]=a[i][0];
-		for(int i=1;i<=n;i++)printf("%.10Lf ",res[i]);
-	}
 
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <numeric>
+#include <cstring>
+#include <climits>
+#include <cassert>
+#include <complex>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <ctime>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#define PB push_back
+// <<= 'Bignum ., //{
+namespace BIGNUM{
+
+const int __base = 10;
+const int MAX_BUF_SIZE = 109;
+char __buf[MAX_BUF_SIZE];
+
+class bignum{
+          friend istream& operator>>(istream&, bignum&);
+          friend ostream& operator<<(ostream&, const bignum&);
+          friend bignum operator +(const bignum&, const bignum&);
+          friend bignum operator -(const bignum&, const bignum&);
+          friend bignum operator *(const bignum&, const bignum&);
+          friend bignum operator /(const bignum&, const bignum&);
+          friend bignum operator %(const bignum&, const bignum&);
+          friend bignum operator +(const bignum&, const int&);
+          friend bignum operator -(const bignum&, const int&);
+          friend bignum operator *(const bignum&, const int&);
+          friend bignum operator /(const bignum&, const int&);
+          friend bignum operator %(const bignum&, const int&);
+          friend bool operator ==(const bignum&, const bignum&);
+          friend bool operator !=(const bignum&, const bignum&);
+          friend bool operator <(const bignum&, const bignum&);
+          friend bool operator >(const bignum&, const bignum&);
+          friend bool operator <=(const bignum&, const bignum&);
+          friend bool operator >=(const bignum&, const bignum&);
+          friend bool operator ==(const bignum&, const int&);
+          friend bool operator !=(const bignum&, const int&);
+          friend bool operator <(const bignum&, const int&);
+          friend bool operator >(const bignum&, const int&);
+          friend bool operator <=(const bignum&, const int&);
+          friend bool operator >=(const bignum&, const int&);
+          friend int do_comp(const bignum&, const int&);
+          friend int do_comp(const bignum&, const bignum&);
+          friend void divide(const bignum&, const bignum&, bignum&, bignum&);
+          friend bignum pow(bignum, int);
+          friend bignum pow(int, int);
+    public:
+        inline bignum(int s = 0){
+            data.clear();
+            while (s) data.PB(s%__base), s/=__base;
+            if (data.empty()) data.PB(0);
+        }
+
+        inline bignum(long long s){
+            data.clear();
+            while (s) data.PB(int(s%__base)), s/=__base;
+            if (data.empty()) data.PB(0);
+        }
+
+        inline bignum(string s){
+            int t, i; data.clear();
+            for (i=int(s.size())-8;i>0;i-=8){
+                istringstream(s.substr(i, 8)) >> t;
+                data.PB(t);
+            }
+            istringstream(s.substr(0, i+8)) >> t;
+            data.PB(t);
+        }
+
+        bignum&operator=(int);
+        bignum&operator=(const string&);
+        bignum&operator=(const bignum&);
+        bignum&operator +=(const bignum&);
+        bignum&operator -=(const bignum&);
+        bignum&operator *=(const bignum&);
+        bignum&operator /=(const bignum&);
+        bignum&operator %=(const bignum&);
+        bignum&operator +=(const int&);
+        bignum&operator -=(const int&);
+        bignum&operator *=(const int&);
+        bignum&operator /=(const int&);
+        bignum&operator %=(const int&);
+        bool undefined();
+        int do_try(const int&);
+        int do_try(const bignum&);
+        void do_trim();
+        list<int> data;
+
+    int size(){
+        list<int>::iterator it; int res = 0;
+        for (it=data.begin(); it!=data.end();it++)
+            res += 1;
+        return res;
+    }
+
+    void do_reserve(int a){
+        if (a <= 0) return;
+        list<int>::iterator it;
+        for (it=data.begin(); it!=data.end() && a>0; it++) a-=8;
+        if (it == data.end() && a>=0) return;
+        a+=8, it--; int f = 1;
+        for (int i=0;i<a;i++) f *= 10; (*it) %= f;
+        data.erase(++it, data.end());
+        do_trim();
+    }
+};
+
+inline bignum&bignum::operator =(const bignum&a){
+    data.clear();
+    for (list<int>::const_iterator i=a.data.begin();i!=a.data.end();i++){
+        data.PB(*i);
+    }
+    return*this;
+}
+inline bignum&bignum::operator =(const string&a){
+    return*this=bignum(a);
+}
+inline bignum&bignum::operator =(int a){
+    return*this=bignum(a);
+}
+
+inline istream& operator>>(istream& input, bignum& a){
+    string s; int t, i; input >> s; a.data.clear();
+    for (i=int(s.size())-8;i>0;i-=8){
+        istringstream(s.substr(i, 8)) >> t;
+        a.data.PB(t);
+    }
+    istringstream(s.substr(0, i+8)) >> t;
+    a.data.PB(t);
+    return input;
+}
+
+inline ostream& operator<<(ostream& output, const bignum& a){
+    list<int>::const_reverse_iterator i=a.data.rbegin();
+    if (*i != 0) output << *i;
+    for (i++;i!=a.data.rend();i++){
+        /*if (*i >= 10000) {
+            if (*i >= 1000000) {if (*i>=10000000) cout << *i; else cout << 0 << *i;}
+            else {if (*i>=100000) cout << "00" << *i; else cout << "000" << *i;}
+        }
+        else {
+            if (*i >= 100) {if (*i>=1000)  cout << "0000" << *i; else cout << "00000" << *i;}
+            else { if (*i>=10) cout << "000000" << *i; else cout << "0000000" << *i;}
+        }*/
+        cout << *i;
+    }
+    return output;
+}
+
+inline bool bignum::undefined(){
+    return data.empty();
+}
+
+inline int do_comp(const bignum& a, const bignum& b){
+    if (a.data.size()<b.data.size()) return -1; if (a.data.size()>b.data.size()) return 1;
+    list<int>::const_reverse_iterator i; list<int>::const_reverse_iterator j;
+    for (i=a.data.rbegin(),j=b.data.rbegin(); j!=b.data.rend(); i++,j++){
+        if (*i<*j) return -1;              //!!!!
+        if (*i>*j) return 1;
+    }
+    return 0;
+}
+inline int do_comp(const bignum& a, const int& b){
+    return do_comp(a, bignum(b));
+}
+
+inline bool operator ==(const bignum& a, const bignum& b){
+    return do_comp(a, b) == 0;
+}
+inline bool operator !=(const bignum& a, const bignum& b){
+    return do_comp(a, b) != 0;
+}
+inline bool operator <(const bignum& a, const bignum& b){
+    return do_comp(a, b) == -1;
+}
+inline bool operator >(const bignum& a, const bignum& b){
+    return do_comp(a, b) == 1;
+}
+inline bool operator <=(const bignum& a, const bignum& b){
+    return do_comp(a, b) != 1;
+}
+inline bool operator >=(const bignum& a, const bignum& b){
+    return do_comp(a, b) != -1;
+}
+
+inline bool operator ==(const bignum& a, const int& b){
+    return do_comp(a, b) == 0;
+}
+inline bool operator !=(const bignum& a, const int& b){
+    return do_comp(a, b) != 0;
+}
+inline bool operator <(const bignum& a, const int& b){
+    return do_comp(a, b) == -1;
+}
+inline bool operator >(const bignum& a, const int& b){
+    return do_comp(a, b) == 1;
+}
+inline bool operator <=(const bignum& a, const int& b){
+    return do_comp(a, b) != 1;
+}
+inline bool operator >=(const bignum& a, const int& b){
+    return do_comp(a, b) != -1;
+}
+
+inline void bignum::do_trim(){
+    while (data.size()>1&&data.back()==0) data.pop_back();
+}
+
+inline bignum& bignum::operator +=(const bignum& a){
+    list<int>::iterator i; list<int>::const_iterator j; int t = 0;
+    for (i=data.begin(),j=a.data.begin(); i!=data.end()&&j!=a.data.end(); i++,j++){
+        *i+=*j+t; t=*i/__base; *i%=__base;
+    }
+    while (i!=data.end()) {*i+=t; t=*i/__base; *i%=__base; i++;}
+    while (j!=a.data.end()) {data.PB(t+*j); t=data.back()/__base; data.back()%=__base; j++;}
+    if (t!=0) data.PB(t);
+    return *this;
+}
+
+inline bignum& bignum::operator -=(const bignum& a){
+    list<int>::iterator i; list<int>::const_iterator j; int t = 0;
+    for (i=data.begin(),j=a.data.begin(); j!=a.data.end(); i++,j++){
+        *i -= t+*j; if (*i>=0) t=0; else *i+=__base, t=1;
+    }
+    while (i!=data.end()) {*i-=t; if (*i>=0) t=0;else *i+=__base, t=1; i++;}
+    (*this).do_trim();
+    return *this;
+}
+
+inline bignum& bignum::operator +=(const int& a){
+    return (*this)+=bignum(a);
+}
+
+inline bignum& bignum::operator -=(const int& a){
+    return (*this)-=bignum(a);
+}
+
+inline bignum operator +(const bignum& a, const bignum& b){
+    list<int>::const_iterator i, j; bignum c; c.data.clear(); int t = 0;
+    for (i=a.data.begin(),j=b.data.begin(); i!=a.data.end()&&j!=b.data.end(); i++,j++){
+        c.data.PB(t+*i+*j);
+        t=c.data.back()/__base; c.data.back()%=__base;
+    }
+    while (i!=a.data.end()) {c.data.PB(t+*i); t=c.data.back()/__base; c.data.back()%=__base; i++;}
+    while (j!=b.data.end()) {c.data.PB(t+*j); t=c.data.back()/__base; c.data.back()%=__base; j++;}
+    if (t!=0) c.data.PB(t);
+    return c;
+}
+
+} //using namespace BIGNUM;
+//}
+
+
+const int N = 105;
+struct bign
+{
+    int len,s[N];
+    bign()  {  memset(s,0,sizeof(s));  len=1;  }
+    bign(int num)  {  *this=num; }
+    bign(char *num) { *this=num; }
+    bign operator =(int num)
+    {
+        char c[N];
+        sprintf(c,"%d",num);
+        *this=c;
+        return *this;
+    }
+    bign operator =(const char *num)
+    {
+        len=strlen(num);
+        for (int i=0;i<len;i++) s[i]=num[len-1-i]-'0';
+        return *this;
+    }
+    string str()
+    {
+        string res="";
+        for (int i=0;i<len;i++) res=(char)(s[i]+'0')+res;
+        return res;
+    }
+    void clean()
+    {
+        while (len>1&&!s[len-1]) len--;
+    }
+    bign operator +(const bign &b)
+    {
+        bign c;
+        c.len=0;
+        for (int i=0,g=0;g||i<len||i<b.len;i++)
+        {
+            int x=g;
+            if (i<len) x+=s[i];
+            if (i<b.len) x+=b.s[i];
+            c.s[c.len++]=x%10;
+            g=x/10;
+        }
+        return c;
+    }
+    bign operator -(const bign &b)
+    {
+        bign c;
+        c.len=0;
+        int x;
+        for (int i=0,g=0;i<len;i++)
+        {
+            x=s[i]-g;
+            if (i<b.len) x-=b.s[i];
+            if (x>=0) g=0;
+            else{
+                x+=10;
+                g=1;
+            };
+            c.s[c.len++]=x;
+        }
+        c.clean();
+        return c;
+    }
+    bign operator *(const bign &b)
+    {
+        bign c;
+        c.len=len+b.len;
+        for (int i=0;i<len;i++) for (int j=0;j<b.len;j++) c.s[i+j]+=s[i]*b.s[j];
+        for (int i=0;i<c.len-1;i++) { c.s[i+1]+=c.s[i]/10; c.s[i]%=10; }
+        c.clean();
+        return c;
+    }
+    bool operator <(const bign &b)
+    {
+        if (len!=b.len) return len<b.len;
+        for (int i=len-1;i>=0;i--)
+             if (s[i]!=b.s[i]) return s[i]<b.s[i];
+        return false;
+    }
+    bign operator +=(const bign &b)
+    {
+        *this=*this+b;
+        return *this;
+    }
+    bign operator -=(const bign &b)
+    {
+        *this=*this-b;
+        return *this;
+    }
+};
+istream& operator >>(istream &in,bign &x)
+{
+  string s;
+  in>>s;
+  x=s.c_str();
+  return in;
+}
+ostream& operator <<(ostream &out,bign &x)
+{
+    out<<x.str();
+    return out;
+}
+
+
+using namespace std;
+const int maxnode=100+20;
+int sigma_size;//后面读入
+struct AC_Automata
+{
+    int ch[maxnode][50+20];
+    int match[maxnode];
+    int f[maxnode];
+    map<char ,int> mp;
+    int sz;
+    void init()
+    {
+        sz=1;
+        memset(ch[0],0,sizeof(ch[0]));
+        match[0]=f[0]=0;
+        mp.clear();
+    }
+    void insert(char *s)
+    {
+        int n=strlen(s),u=0;
+        for(int i=0;i<n;i++)
+        {
+            int id=mp[s[i]];
+            if(ch[u][id]==0)
+            {
+                ch[u][id]=sz;
+                memset(ch[sz],0,sizeof(ch[sz]));
+                match[sz++]=0;
+            }
+            u=ch[u][id];
+        }
+        match[u]=1;
+    }
+    void getFail()
+    {
+        f[0]=0;
+        queue<int> q;
+        for(int i=0;i<sigma_size;i++)
+        {
+            int u=ch[0][i];
+            if(u)
+            {
+                f[u]=0;
+                q.push(u);
+            }
+        }
+        while(!q.empty())
+        {
+            int r=q.front();q.pop();
+            for(int i=0;i<sigma_size;i++)
+            {
+                int u=ch[r][i];
+                if(!u){ ch[r][i]=ch[f[r]][i]; continue; }
+                q.push(u);
+                int v=f[r];
+                while(v && ch[v][i]==0) v=f[v];
+                f[u]=ch[v][i];
+                match[u] |= match[f[u]];
+            }
+        }
+    }
+};
+AC_Automata ac;
+using namespace BIGNUM;
+bign dp[100+20][50+10];
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+
+
+    int n,m,p;
+    char str[50+20];
+    scanf("%d%d%d",&n,&m,&p);
+
+        sigma_size=n;
+        ac.init();
+        scanf("%s",str);
+        for(int i=0;i<n;i++)
+            ac.mp[str[i]]=i;
+        while(p--)
+        {
+            scanf("%s",str);
+            ac.insert(str);
+        }
+        ac.getFail();
+
+
+
+        dp[0][0]=bign(1);//起点
+        for(int k=0;k<m;k++)
+        {
+            for(int i=0;i<ac.sz;i++)
+                if(ac.match[i]==0) {
+                for(int j=0;j<sigma_size;j++)  if(ac.match[ac.ch[i][j]]==0)
+                        dp[ac.ch[i][j]][k+1] = dp[ac.ch[i][j]][k+1] + dp[i][k];
+                }
+        }
+        bign z = 0;
+        for(int i=0;i<ac.sz;i++) if(ac.match[i]==0)
+			z = z + dp[i][m];
+        cout << z;
 }
--- Workspace/Cpp/Template/几何.cpp
+++ Workspace/Cpp/Template/几何.cpp
@@ -1,19 +1,19 @@
 // <<= '9. Comutational Geometry .,//{
-namespace CG{
-    
+namespace geometry{
+
 #define cPo const Po&
 #define cLine const Line&
 #define cSeg const Seg&
-    
+
     inline DB dist2(DB x,DB y){return sqr(x)+sqr(y);}
-    
+
     struct Po{
         DB x,y;Po(DB x=0,DB y=0):x(x),y(y){}
-        
+
         void in(){RF(x,y);}void out(){printf("(%.2f,%.2f)",x,y);}
         inline friend istream&operator>>(istream&i,Po&p){return i>>p.x>>p.y;}
         inline friend ostream&operator<<(ostream&o,Po p){return o<<"("<<p.x<<", "<<p.y<< ")";}
-        
+
         Po operator-()const{return Po(-x,-y);}
         Po&operator+=(cPo p){x+=p.x,y+=p.y;rTs;}Po&operator-=(cPo p){x-=p.x,y-=p.y;rTs;}
         Po&operator*=(DB k){x*=k,y*=k;rTs;}Po&operator/=(DB k){x/=k,y/=k;rTs;}
@@ -21,16 +21,16 @@ namespace CG{
         Po operator+(cPo p)const{return Po(x+p.x,y+p.y);}Po operator-(cPo p)const{return Po(x-p.x,y-p.y);}
         Po operator*(DB k)const{return Po(x*k,y*k);}Po operator/(DB k)const{return Po(x/k,y/k);}
         Po operator*(cPo p)const{return Po(x*p.x-y*p.y,y*p.x+x*p.y);}Po operator/(cPo p)const{return Po(x*p.x+y*p.y,y*p.x-x*p.y)/p.len2();}
-        
+
         bool operator==(cPo p)const{return!sgn(x,p.x)&&!sgn(y,p.y);};bool operator!=(cPo p)const{return sgn(x,p.x)||sgn(y,p.y);}
         bool operator<(cPo p)const{return sgn(x,p.x)<0||!sgn(x,p.x)&&sgn(y,p.y)<0;}bool operator<=(cPo p)const{return sgn(x,p.x)<0||!sgn(x,p.x)&&sgn(y,p.y)<=0;}
         bool operator>(cPo p)const{return!(Ts<=p);}bool operator >=(cPo p)const{return!(Ts<p);}
-        
+
         DB len2()const{return dist2(x,y);}DB len()const{return sqrt(len2());}DB arg()const{return atan2(y,x);}
         Po&_1(){rTs/=len();}Po&conj(){y=-y;rTs;}Po&lt(){swap(x,y),x=-x;rTs;}Po&rt(){swap(x,y),y=-y;rTs;}
         Po&rot(DB a,cPo o=Po()){Ts-=o;Ts*=Po(cos(a),sin(a));rTs+=o;}
-        
-        
+
+
         // 求所在象限，大部分情况下只区分两个足够。
         inline int q()const{
             return (y > 0 || y == 0 && x >= 0) ? 0 : 1;
@@ -40,7 +40,7 @@ namespace CG{
              return 3;*/
         }
     };
-    
+
     inline DB dot(DB x1,DB y1,DB x2,DB y2){return x1*x2+y1*y2;}
     inline DB dot(cPo a,cPo b){return dot(a.x,a.y,b.x,b.y);}
     inline DB dot(cPo p0,cPo p1,cPo p2){return dot(p1-p0,p2-p0);}
@@ -67,48 +67,48 @@ namespace CG{
     inline Po rot(Po p,DB a,cPo o=Po()){return p.rot(a,o);}
     inline Po operator *(DB k,cPo p){return p*k;}
     inline Po operator /(DB k,cPo p){return conj(p)*k/p.len2();}
-    
+
     typedef vector<Po> VP;
-    
+
     struct Line{
         Po a,b;Line(cPo a=Po(),cPo b=Po()):a(a),b(b){}
         Line(DB x0,DB y0,DB x1,DB y1):a(Po(x0,y0)),b(Po(x1,y1)){}
         Line(cLine l):a(l.a),b(l.b){}
-        
+
         //Ax+By+C=0
         Line(DB A,DB B,DB C){
             C=-C;if(!::sgn(A))a=Po(0,C/B),b=Po(1,C/B);
             else if(!::sgn(B))a=Po(C/A,0),b=Po(C/A,1);
             else a=Po(0,C/B),b=Po(1,(C-A)/B);
         }
-        
+
         void in(){a.in(),b.in();}
         inline friend istream&operator>>(istream&i,Line& p){return i>>p.a>>p.b;}
         inline friend ostream&operator<<(ostream&o,Line p){return o<<p.a<<"-"<< p.b;}
-        
+
         Line operator+(cPo x)const{return Line(a+x,b+x);}
         Line operator-(cPo x)const{return Line(a-x,b-x);}
         Line operator*(DB k)const{return Line(a*k,b*k);}
         Line operator/(DB k)const{return Line(a/k,b/k);}
-        
+
         Po operator*(cLine)const;
         Po d()const{return b-a;}DB len2()const{return d().len2();}DB len()const{return d().len();}DB arg()const{return d().arg();}
-        
+
         int sgn(cPo p)const{return dett(a, b, p);}
         int sgn(cLine)const;
-        
+
         bool sameSgn(cPo  p1,cPo  p2)const{return sgn(p1)==sgn(p2);}
         void getEquation(DB&K,DB&B)const{
             K = ::sgn(a.x, b.x) ? (b.y-a.y)/(b.x-a.x) : OO;
             B = a.y - K*a.x;
         }
         void getEquation(DB&A,DB&B,DB&C)const{A=a.y-b.y,B=b.x-a.x,C=det(a, b);}
-        
+
         Line&push(DB r){ // 正数右手螺旋向里
             Po v=d()._1().lt()*r;a+=v,b+=v; rTs;
         }
     };
-    
+
     inline DB dot(cLine l1,cLine l2){return dot(l1.d(),l2.d());}
     inline DB dot(cLine l,cPo p){return dot(l.a,l.b,p);}
     inline DB dot(cPo p,cLine l){return dot(p,l.a,l.b);}
@@ -118,73 +118,73 @@ namespace CG{
     inline DB ang(cLine l0,cLine l1){return ang(l0.d(),l1.d());}
     inline DB ang(cLine l,cPo p){return ang(l.a,l.b,p);}
     inline DB ang(cPo p,cLine l){return ang(p,l.a,l.b);}
-    
+
     inline int Line::sgn(cLine l)const{return dett(Ts, l);}
     inline Po Line::operator*(cLine l)const{return a+d()*det(a,l)/det(Ts,l);}
     inline Po operator&(cPo p,cLine l){return l*Line(p,p+l.d().lt());}
     inline Po operator%(cPo p,cLine l){return p&l*2-p;}
     inline Line push(Line l, DB r){return l.push(r);}
-    
-    
+
+
     struct Seg: public Line{
         Seg(cPo a=Po(),cPo b=Po()):Line(a,b){}
         Seg(DB x0,DB y0,DB x1,DB y1):Line(x0,y0,x1,y1){}
         Seg(cLine l):Line(l){}
         Seg(const Po &a,DB alpha):Line(a,alpha){}
         Seg(DB A,DB B,DB C):Line(A,B,C){}
-        
+
         inline int sgn(cPo p)const;
         inline int sgn(cLine l)const;
         inline bool qrt(cSeg l)const;
         inline int sgn(cSeg l)const;
     };
-    
+
     // -1不相交 0相交（不规范） 1相交（规范）
-    
+
     //inline int Seg::sgn(cPo p)const{return -dott(p,a,b);}
-    
+
     inline int Seg::sgn(cPo p)const{
         if (dett(p, a, b)) return -1; // 有时会有精度误差。。
         if (a == p || b == p) return 0;
         return -dott(p,a,b);
     }
-    
-    
+
+
     inline int Seg::sgn(cLine l)const{return sgn(Ts*l);}
-    
+
     // quick_rejection_test
     inline bool Seg::qrt(cSeg l)const{
         return min(a.x,b.x)<=max(l.a.x,l.b.x)&&min(l.a.x,l.b.x)<=max(a.x,b.x)&&
         min(a.y,b.y)<=max(l.a.y,l.b.y)&&min(l.a.y,l.b.y)<=max(a.y,b.y);
     }
-    
-    
+
+
     inline int Seg::sgn(cSeg l)const{
         if (!qrt(l)) return -1;
-        
+
         /*return
          (dett(a,b,l.a)*dett(a,b,l.b)<=0 &&
          dett(l.a,l.b,a)*dett(l.a,l.b,b)<=0)?1:-1;*/
-        
+
         int d1=dett(a,b,l.a),d2=dett(a,b,l.b),d3=dett(l.a,l.b,a),d4=dett(l.a,l.b,b);
         if ((d1^d2)==-2&&(d3^d4)==-2)return 1;
         return ((!d1&&dott(l.a-a,l.a-b)<=0)||(!d2&&dott(l.b-a,l.b-b)<=0)||
                 (!d3&&dott(a-l.a,a-l.b)<=0)||(!d4&&dott(b-l.a,b-l.b)<=0))?0:-1;
     }
-    
+
     //inline DB dist2(cLine l,cPo p){return sqr(fabs(dot(lt(l.d()), p-l.a)))/l.len2();}
     inline DB dist2(cLine l,cPo p){return sqr(fabs(det(l.d(), p-l.a)))/l.len2();}
-    
+
     inline DB dist2(cLine l1,cLine l2){return dett(l1,l2)?0:dist2(l1,l2.a);}
-    
+
     inline DB dist2(cSeg l,cPo p){
         Po pa = p - l.a, pb = p - l.b;
         if (dott(l.d(), pa) <= 0) return pa.len2();
         if (dott(l.d(), pb) >= 0) return pb.len2();
         return dist2(Line(l), p);
     }
-    
-    
+
+
     inline DB dist2(cSeg s,cLine l){
         Po v1=s.a-l.a,v2=s.b-l.a;DB d1=det(l.d(),v1),d2=det(l.d(),v2);
         return sgn(d1)!=sgn(d2) ? 0 : sqr(min(fabs(d1), fabs(d2)))/l.len2();
@@ -196,5 +196,5 @@ namespace CG{
     template<class T1, class T2> inline DB dist2(const T1& a, const T2& b){
         return dist2(b, a);
     }
-    
-} using namespace CG;//}
+
+} using namespace geometry;//}
