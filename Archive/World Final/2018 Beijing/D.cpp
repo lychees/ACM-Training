455d06a1b9d84e247124bb6abf84c9cd4f2a7f78
xiaodao <lychees67@gmail.com>
2022-11-17 07:20:47 +0800

    go

---
+++ Archive/World Final/2018 Beijing/D.cpp
@@ -0,0 +1,40 @@
+#include <lastweapon/io>
+using namespace lastweapon;
+
+
+const int N = int(1e3) + 9;
+DB F[N][N], G[N][N], C[N][N];
+int n, m, r;
+
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+#endif
+
+    RD(n,m,r);
+
+    REP(i, n+m) {
+        C[i][0] = 1;
+        REP(j, i) C[i][j+1] = C[i-1][j] + C[i-1][j+1];
+    }
+
+    G[0][n] = 1;
+
+#define gu G[i][j]
+#define fu F[i][j]
+
+    REP(i, m) {
+        REP(j, n+1) if (sgn(gu)) {
+            REP_1(d, min(j, m-i)) {
+                DB c = C[j][d], delta = gu * c;
+                G[i+d][d] += delta;
+                F[i+d][d] += fu * c + delta * min(d, r);
+            }
+        }
+    }
+
+    printf("%.7f\n", accumulate(F[m], F[m]+n+1, .0)/C[n-1+m][m] + r);
+}
--- Archive/World Final/README.md
+++ Archive/World Final/README.md
@@ -1,3 +1,14 @@
+# 2017
+https://cekavis.github.io/icpc-world-finals-2017/
+
+# 2018
+https://foreverlasting1202.github.io/2019/04/18/ICPC%20World%20Finals%202018/
+https://www.csc.kth.se/~austrin/icpc/finals2018solutions.pdf
+
+# 2019
+http://littledva.cn/article-18/
+
+
 ## Ref
 - https://icpc.baylor.edu/worldfinals/problems
 - https://github.com/SnapDragon64/ACMFinalsSolutions
